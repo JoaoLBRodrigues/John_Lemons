0ed72cff724edbb0595e9c98c4b023509dd485fc
João Rodrigues <rodrigues2001mdl@gmail.com>
2023-04-28 18:15:08 +0100

    ghost

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,30 +1,21 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
     public GameObject player;
-    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup exitBackgroundImageCanvasGroup; 
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update ()
-    {
-        if(m_IsPlayerAtExit)
-        {
-            EndLevel ();
-        }
-    }
+    
     void OnTriggerEnter (Collider other)
     {
         if (other.gameObject == player)
@@ -32,15 +23,39 @@ public class GameEnding : MonoBehaviour
             m_IsPlayerAtExit = true;
         }
     }
-    void EndLevel ()
+
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    void Update ()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel (exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
+        }
+    }
+
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
-
-        if(m_Timer > fadeDuration + displayImageDuration)
+        if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit ();
+            if (doRestart)
+            {
+                SceneManager.LoadScene (0);
+            }
+            else
+            {
+                Application.Quit ();
+            }
         }
     }
 }
